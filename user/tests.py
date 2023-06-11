bf45c49adab3cfdcf9754fedf52741d4196c8cb3
Magda Sokolowska <magdalenasokolowska2307@gmail.com>
2023-06-11 21:09:13 +0200

    refactor test.py

--- user/tests.py
+++ user/tests.py
@@ -1,42 +1,26 @@
-from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APIClient
-from .models import User
-
-from django.urls import reverse
 from rest_framework.test import APITestCase
-from rest_framework import status
-from .models import User
-
-from django.urls import reverse
-from rest_framework.test import APITestCase
-from rest_framework import status
 from .models import User
 
 class UserRegistrationLoginTestCase(APITestCase):
-    def test_user_registration(self):
-        url = reverse('user-registration')
-        data = {
-            'email': 'test@example.com',
-            'password': 'password123',
-        }
-
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(User.objects.count(), 1)
-        self.assertEqual(User.objects.get().email, 'test@example.com')
-        self.assertNotEqual(User.objects.get().password, 'password123')
-
-    def test_user_login(self):
-        data = {
+    def setUp(self):
+        self.registration_url = reverse('user-registration')
+        self.login_url = reverse('user-login')
+        self.user_data = {
             'email': 'test@example.com',
             'password': 'password123'
         }
+        self.user = User.objects.create_user(**self.user_data)
 
-        User.objects.create_user(**data)
-        url = reverse('user-login')
+    def test_user_registration(self):
+        response = self.client.post(self.registration_url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 2)  # Adjusted count to include initial user
+        self.assertEqual(User.objects.last().email, 'test@example.com')
+        self.assertNotEqual(User.objects.last().password, 'password123')
 
-        response = self.client.post(url, data, format='json')
+    def test_user_login(self):
+        response = self.client.post(self.login_url, self.user_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('token', response.data)
