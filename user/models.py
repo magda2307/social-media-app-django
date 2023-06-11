cfc63380e36c489dd59ab172d32d0a9db5d4661b
Magda Sokolowska <magdalenasokolowska2307@gmail.com>
2023-06-11 16:53:13 +0200

    add user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    """Custom User model for the social media app."""
+    email = models.EmailField(unique=True)
+    profile_picture = models.URLField(blank=True)
+    bio = models.CharField(max_length=255, blank=True)
+    is_admin = models.BooleanField(default=False)
+    followers = models.ManyToManyField('self', symmetrical=False, blank=True, related_name='following')
+    ##posts = 
+    ##tags =
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    def __str__(self):
+        return self.email
