67cff2f44d5a5c41c07a1613a2c96f8a703497e1
Magda Sokolowska <magdalenasokolowska2307@gmail.com>
2023-06-23 16:56:14 +0200

    add image fields, create prepare_image_and_save_in_temp helper function for models.py

--- user/models.py
+++ user/models.py
@@ -1,9 +1,29 @@
+from io import BytesIO
+import os
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-
+from PIL import Image
+import settings
 #Constants at the module level, may be moved to constants.py in future
 MAX_TAG_LENGTH = 50
+PROFILE_PICS_UPLOAD_PATH = 'profile_pics'
+POST_IMAGES_UPLOAD_PATH = 'post_images'
+PROFILE_PIC_SIZE_TUPLE = (300, 300)
 
+
+def prepare_image_and_save_in_temp(image, size_tuple=PROFILE_PIC_SIZE_TUPLE):
+    """Helper function to resize the image and to get the temporary file path."""
+    img = Image.open(image)
+    img.thumbnail(size_tuple)
+    output = BytesIO()
+    img.save(output, format='JPEG', quality=80)
+    output.seek(0)
+    
+    temp_file_path = os.join(settings.MEDIA_ROOT, 'temp', f"{image.name.split('.')[0]}.jpg")
+
+    with open(temp_file_path,'wb') as temp_file:
+        temp_file.write(output.getvalue())
+    return temp_file_path
 
 class UserManager(BaseUserManager):
     """Manager for users in the system."""
@@ -32,7 +52,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     """Custom User model for the social media app."""
     
     email = models.EmailField(unique=True)
-    profile_picture = models.URLField(blank=True)
+    profile_picture = models.ImageField(upload_to=PROFILE_PICS_UPLOAD_PATH, blank=True)
     bio = models.CharField(max_length=255, blank=True)
     is_staff = models.BooleanField(default=False)
     followers = models.ManyToManyField('self', symmetrical=False, blank=True, related_name='following')
@@ -48,13 +68,21 @@ class Post(models.Model):
     """Post model for the social media app."""
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     text = models.CharField(max_length=255, blank=False)
-    image = models.URLField(blank=True)
+    image = models.ImageField(upload_to=POST_IMAGES_UPLOAD_PATH, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
     likes = models.ManyToManyField(User, blank=True, related_name='liked_posts')
 
     def __str__(self):
         return self.text
+
+    def save(self, *args, **kwargs):
+        if self.profile_picture:
+            temp_file_path
+            permament_file_path
+            self.move_image_to_permament
+        
+        super().save(*args, **kwargs)
 
 class Tag(models.Model):
     """Tag model for the social media app. 
